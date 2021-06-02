9da134f5b4c5a3e5ff795ed93cb2497dafb5b224
jesvinabiju <84664087+jesvinabiju@users.noreply.github.com>
2021-06-02 16:26:58 +0300

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -42,18 +42,19 @@ int main(void) {
 float mc_pi(int n)
 {	
 	int pc=0,ps=n;
-
+	
 	for(int i=0; i<n; i++){
-	float x,y,l;
+	float x,y,len;
 	x=frandom();
 	y=frandom();
 	len =x*x+y*y;
-	if(l<=1){
+	if(len<=1){
 	pc++;
 	}
 	}
 	float pi = 4.0*pc/ps;
 	return pi;
 }
+
 
 
